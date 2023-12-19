49a9b5cbe1e2eb6653fabed680eca748fcb70994
Aum Kansara <117375317+Aum-Kansara@users.noreply.github.com>
2023-12-19 18:42:32 +0000

    Added neccesary comments

--- responder.py
+++ responder.py
@@ -2,16 +2,27 @@ from transformers import pipeline,BertForQuestionAnswering,AutoTokenizer
 from random import choice
 from requests import get
 from web_parser import parser
+from dotenv import load_dotenv
+from os import getenv
 
-SEARCH_ENGINE_ID="d6e0e4194507c4642"
-API_KEY="AIzaSyA-vkDczHM8JLKrdER_BcPADXx5uOJjQeo"
+# Load Enviroment Variables
+load_dotenv()
+
+
+# Get search engine ID (Programmable Search Engine)
+SEARCH_ENGINE_ID=getenv("SEARCH_ENGINE_ID")
+API_KEY=getenv("API_KEY")
 google_api_url="https://www.googleapis.com/customsearch/v1"
 
+
+# Load Question Answer Model from HuggingFace
 QA_MODEL=BertForQuestionAnswering.from_pretrained('deepset/bert-base-cased-squad2')
 tokenizer=AutoTokenizer.from_pretrained('deepset/bert-base-cased-squad2')
-QA_PIPELINE=pipeline("question-answering",model='deepset/bert-base-cased-squad2',tokenizer='deepset/bert-base-cased-squad2')
 
+# Create Pipeline for QA model
+QA_PIPELINE=pipeline("question-answering",model=QA_MODEL,tokenizer=tokenizer)
 
+# Create Pipeline for Text Classification
 pipe = pipeline("text-classification", model="dipesh/Intent-Classification-Bert-Base-Cased")
 intents={'greet':["Hey! I appreciate the friendly hello. What's your question?","Hi there! Thanks for the greeting. Is there a particular query or question on your mind?","Hello! I'm at your service. Any specific questions or topics you'd like to explore about science?","Greetings! Your hello is welcomed. What's your question or inquiry for today?","Hey! I'm here for you. Do you have a query or something you'd like to discuss?","Hi! Thanks for reaching out. What can I help you with today? Any specific questions?","Hey! Your greeting is much appreciated. Do you have a question or something you'd like to know?"],
          'goodbye':["Goodbye! Feel free to return anytime.",
@@ -24,6 +35,7 @@ intents={'greet':["Hey! I appreciate the friendly hello. What's your question?",
                     "Adios! Need anything else?",
                     "Bye! Until we meet again."]}
 
+# Method for getting output message
 def getResponse(text):
     detected_intent=None
     answer_found=False
@@ -32,7 +44,8 @@ def getResponse(text):
     conversation+=usr_inp+"\n"
     result=pipe(usr_inp)[0]
 
-    if result['score']>0.5:
+    # Check if Classified intent is 'greet' or 'goodbye'
+    if result['score']>0.5: # 0.5 is confidence level
         detected_intent=result['label']
         if detected_intent in intents.keys():
             res=choice(intents[detected_intent])
@@ -40,15 +53,15 @@ def getResponse(text):
             answer_found=True
             return res
         
-    else:
     # Check if Answer availble in previous conversation
-        with open("page_content.txt",'r') as f:
+    else:
+        with open("page_content.txt",'r') as f:  # Open Page_content file 
             QA_input={
                 'question':usr_inp,
                 'context': f.read()
             }
-            qa_res=QA_PIPELINE(QA_input)
-            if qa_res['score']>0.1:
+            qa_res=QA_PIPELINE(QA_input) # Try to find answer from Page_content file
+            if qa_res['score']>0.1:  # If confidence is > 0.1
                 conversation+=f"Assistant : {qa_res['answer']}"
                 answer_found=True
                 return qa_res['answer']
@@ -60,16 +73,14 @@ def getResponse(text):
                 'key':API_KEY,
                 'cx':SEARCH_ENGINE_ID,
             }
-            
 
-
-            response=get(google_api_url,params=params)
-            results=response.json()['items']
-            if len(results)>0:
+            response=get(google_api_url,params=params)  # Make GET request on search engine
+            results=response.json()['items']  # retrive first search item
+            if len(results)>0: # if search item present
                 link=results[0]['link']
-                final_text=parser(link)
+                final_text=parser(link)   # parse the output, remove html tags
 
-                with open("page_content.txt",'a') as f:
+                with open("page_content.txt",'a') as f:  # append text to file for future conversation
                     f.write(f"\n\n{usr_inp}\n")
                     f.write(final_text+"\n\n")
                 
@@ -78,12 +89,12 @@ def getResponse(text):
                     'context': final_text
                 }
                 qa_res=QA_PIPELINE(QA_input)
-                if qa_res['score']>0.1:
+                if qa_res['score']>0.1:  # if confidence score is > 0.1 return answer
                     conversation+=f"Assistant : {qa_res['answer']}"
                     return qa_res['answer']
-                else:
+                else: # else return any one of this answers
                     return choice(['sorry! can\'t answer that','I don\'t know','No idea about that','Don\'t know!'])
-            else:
+            else: # else return one random answer from list
                 return choice(['sorry! can\'t answer that','I don\'t know','No idea about that','Don\'t know!'])
     return choice(['sorry! can\'t answer that','I don\'t know','No idea about that','Don\'t know!'])        
     
--- web_parser.py
+++ web_parser.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 from wikipedia import summary,search
 
 
+# Scrapes content of given link and parse it to find relevant content
 def parser(url):
     res=get(url)
     soup = BeautifulSoup(res.content, 'html.parser') 
-    if 'spaceplace.nasa.gov' in url:
+    if 'spaceplace.nasa.gov' in url:  # if given url is of nasa.com
         s=soup.find('div',id='main')
         s=s.get_text()
         lines = (line.strip() for line in s.splitlines())
@@ -14,12 +15,12 @@ def parser(url):
         text = '\n'.join(chunk for chunk in chunks if chunk)
         return text
 
-    elif 'en.wikipedia.org' in url:
+    elif 'en.wikipedia.org' in url:  # if given url is of wikipedia
         results = search(url.split('/')[-1])
         text=summary(results[0],auto_suggest=False)
         return text
 
-    elif 'byjus.com' in url:
+    elif 'byjus.com' in url:  # if given url of byjus.com
         s=soup.find('div',class_='bgc-white p30 mb20 pm15')
         s=s.find_all('p')
         text=""
